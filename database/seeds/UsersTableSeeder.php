1c2338cd46321664bff49ebc5964102575c80c77
minas1986 <30533267+minas1986@users.noreply.github.com>
2019-03-16 16:41:39 +0200

    Added verify and user logic

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: postp
+ * Date: 16.03.2019
+ * Time: 16:04
+ */
---
+++ resources/views/main.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: postp
+ * Date: 15.03.2019
+ * Time: 22:29
+ */
